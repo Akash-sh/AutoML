9a8c13c9629687e1ff10be2ac096130cf114de0e
Akash-sh <hereisakash01@gmail.com>
2023-08-03 01:03:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 import streamlit as st
 import pandas as pd
 from lazypredict.Supervised import LazyRegressor
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
